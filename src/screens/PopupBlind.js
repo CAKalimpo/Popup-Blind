857861aa04362f03bc09a0d379845ee289898d31
Czarina Angelica Kalimpo <cakalimpo@gmail.com>
2024-05-08 13:34:51 +0800

    updates

--- src/screens/PopupBlind.js
+++ src/screens/PopupBlind.js
@@ -1,11 +1,59 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { Modal, Text, Pressable, View, Image, ScrollView } from 'react-native';
+import { styles } from '../style';
+import closeIcon from '../../assets/imgs/close.png';
 
-const PopUpBlind = () => (
-    <View>
-        <Text>
-            May crush aq sa faculty
-        </Text>
-    </View>
-);
-export default PopUpBlind;
+const App = () => {
+    const [modalVisible, setModalVisible] = useState(false);
+
+    return (
+        <View style={styles.centeredView}>
+            <Modal
+                animationType="slide"
+                transparent
+                visible={modalVisible}
+                onRequestClose={() => {
+                    setModalVisible(!modalVisible);
+                }}
+            >
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <View style={styles.modalHeader}>
+                            <Text style={styles.modalText}>Blinds Structure</Text>
+                            <Pressable
+                                style={[styles.button, styles.buttonClose]}
+                                onPress={() => setModalVisible(!modalVisible)}
+                            >
+                                <Image
+                                    source={closeIcon}
+                                    style={{ width: 20, height: 20 }}
+                                />
+                            </Pressable>
+                        </View>
+                        <ScrollView style={styles.svStyle}>
+                            <View style={styles.row}>
+                                <View style={styles.column}>
+                                    <Text style={styles.text}>Level</Text>
+                                </View>
+                                <View style={styles.column}>
+                                    <Text style={styles.text}>Time</Text>
+                                </View>
+                                <View style={styles.column}>
+                                    <Text style={styles.text}>Blinds</Text>
+                                </View>
+                            </View>
+                        </ScrollView>
+                    </View>
+                </View>
+            </Modal>
+            <Pressable
+                style={[styles.button, styles.buttonOpen]}
+                onPress={() => setModalVisible(true)}
+            >
+                <Text style={styles.textStyle}>Show Modal</Text>
+            </Pressable>
+        </View>
+    );
+};
+
+export default App;
--- src/style.js
+++ src/style.js
@@ -1,13 +1,76 @@
 import { Fonts } from './CustomFonts';
 
 export const styles = {
-    container: {
+    centeredView: {
         flex: 1,
+        alignItems: 'center',
+        marginTop: 15,
     },
-    title: {
-        fontFamily: Fonts.ISM,
-        color: 'black',
+    modalView: {
+        backgroundColor: '#131928',
+        borderRadius: 5,
+        padding: 20,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+        width: '90%',
+    },
+    modalHeader: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        top: -20,
+        left: 10,
+        alignItems: 'center',
+        width: '100%',
+        marginBottom: 20,
+    },
+    button: {
+        borderRadius: 20,
+        padding: 10,
+    },
+    buttonOpen: {
+        backgroundColor: '#F194FF',
+    },
+    buttonClose: {
+        backgroundColor: 'transparent',
+        left: 10,
+    },
+    textStyle: {
+        color: 'white',
+        fontWeight: 'bold',
         textAlign: 'center',
-        marginBottom: 10,
+    },
+    modalText: {
+        textAlign: 'center',
+        flex: 1,
+        fontFamily: Fonts.SFPro,
+        color: 'white',
+        fontSize: 16,
+    },
+    svStyle: {
+        top: -40,
+        width: '105%',
+        backgroundColor: '#101523',
+    },
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: 2,
+        margin: 5,
+    },
+    text: {
+        color: 'white',
+        textAlign: 'center',
+        justifyContent: 'space-between',
+        fontFamily: Fonts.ISMR,
+    },
+    column: {
+        alignItems: 'center',
     },
 };
