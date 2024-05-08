abedd55261d771736fab33937f123030a2ca666e
Czarina Angelica Kalimpo <cakalimpo@gmail.com>
2024-05-08 10:54:38 +0800

    Initialize

---
+++ App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import PopUpBlind from './src/screens/PopupBlind';
+
+const App = () => (
+    <PopUpBlind/>
+);
+export default App;
---
+++ src/style.js
@@ -0,0 +1,13 @@
+import { Fonts } from './CustomFonts';
+
+export const styles = {
+    container: {
+        flex: 1,
+    },
+    title: {
+        fontFamily: Fonts.ISM,
+        color: 'black',
+        textAlign: 'center',
+        marginBottom: 10,
+    },
+};
